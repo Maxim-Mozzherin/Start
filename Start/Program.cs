ccfae8045ad28300775772ac1add7a8261e9f303
Maxim A. Mozzherin <m.mozzherin.job@outlook.com>
2024-11-22 14:38:12 +0500

    Виды ошибок при работе с константами, а так же объявление переменных

--- Start/Program.cs
+++ Start/Program.cs
@@ -1,3 +1,17 @@
-﻿Console.Write("Введите свое имя: ");
-var name = Console.ReadLine();       // вводим имя
-Console.WriteLine($"Привет, {name}!");    // выводим имя на консоль
+﻿string name = "Tom";  // определяем переменную и инициализируем ее
+
+Console.WriteLine(name);    // Tom
+
+name = "Bob";       // меняем значение переменной
+Console.WriteLine(name);    // Bob
+
+
+// Константы
+
+
+const string NAME = "Tom";  // определяем константу
+/*  -----Пример ошибки-----
+1.     const string NAME;  // ! Ошибка - константа NAME не инициализирована
+2.     const string NAME = "Tom";  // определяем константу
+NAME = "Bob";   // !Ошибка - у констаты нельзя изменить значение 
+*/
