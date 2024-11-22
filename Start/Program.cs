dfd8551262dc98ff37522abc1fe3eeb82efb528a
Maxim A. Mozzherin <m.mozzherin.job@outlook.com>
2024-11-22 15:08:46 +0500

    Использование литералов

--- Start/Program.cs
+++ Start/Program.cs
@@ -1,17 +1,39 @@
-﻿string name = "Tom";  // определяем переменную и инициализируем ее
+﻿//Логические литералы
+//Есть две логических константы - true (истина) и false (ложь):
+Console.WriteLine(true);
+Console.WriteLine(false);
+// Целочисленные литералы 
+Console.WriteLine(-11);
+Console.WriteLine(5);
+Console.WriteLine(505);
+// Целочисленные литералы (двоичная)
+Console.WriteLine(0b11);        // 3
+Console.WriteLine(0b1011);      // 11
+Console.WriteLine(0b100001);    // 33
+// Целочисленные литералы (16тиричная)
+Console.WriteLine(0x0A);    // 10
+Console.WriteLine(0xFF);    // 255
+Console.WriteLine(0xA1);    // 161
+//Вещественные литералы
+Console.WriteLine(3.14); 
+Console.WriteLine(1.2);
+//Вещественные литералы (Экспоненциальные)
+Console.WriteLine(3.2e3);   // по сути равно 3.2 * 10<sup>3</sup> = 3200 (Экспоненциальные)
+Console.WriteLine(1.2E-1);  // равно 1.2 * 10<sup>-1</sup> = 0.12
+//Символьные литералы
+Console.WriteLine('2');
+Console.WriteLine('A');
+Console.WriteLine('T');
 
-Console.WriteLine(name);    // Tom
+Console.WriteLine('\x78');    // x
+Console.WriteLine('\x5A');    // Z
 
-name = "Bob";       // меняем значение переменной
-Console.WriteLine(name);    // Bob
+Console.WriteLine('\u0420');    // Р
+Console.WriteLine('\u0421');    // С
+//Строковые литералы
+Console.WriteLine("hello");
+Console.WriteLine("фыва");
+Console.WriteLine("hello word");
 
 
-// Константы
-
-
-const string NAME = "Tom";  // определяем константу
-/*  -----Пример ошибки-----
-1.     const string NAME;  // ! Ошибка - константа NAME не инициализирована
-2.     const string NAME = "Tom";  // определяем константу
-NAME = "Bob";   // !Ошибка - у констаты нельзя изменить значение 
-*/
+Console.WriteLine("Привет \nмир");
