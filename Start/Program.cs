050dcb28aece780e74ef0d079258b08033457425
Maxim A. Mozzherin <m.mozzherin.job@outlook.com>
2024-12-22 15:26:55 +0500

    Фибоначчи и факторилы

--- Start/Program.cs
+++ Start/Program.cs
@@ -1,66 +1,101 @@
-﻿// работа с массивами
-int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
-int result = 0;
-foreach (int number in numbers)
-{
-    if (number > 0)
-    {
-        result++;
-    }
-}
-Console.WriteLine($"Число элементов больше нуля: {result}");
+﻿// Рекурсивная функция
 
-//Инверсия массива 
-int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
 
-int n = numbers.Length; // длина массива
-int k = n / 2;          // середина массива
-int temp;               // вспомогательный элемент для обмена значениями
-for (int i = 0; i < k; i++)
+//Метод для нахождения факториала
+int Factorial(int n)
 {
-    temp = numbers[i];
-    numbers[i] = numbers[n - i - 1];
-    numbers[n - i - 1] = temp;
-}
-foreach (int i in numbers)
-{
-    Console.Write($"{i} \t");
-}
-// так же есть встроенный метод для реверса Array.Revers, не понимаю, почему его нет в примерах Метанит
-int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
-Array.Reverse(numbers); // встроенный метод для разворота массива
-foreach (int i in numbers)
-{
-    Console.Write($"{i} \t");
-}
-// Сортировка массива "руками"
-int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
+    if (n == 1) return 1;
 
-// сортировка
-int temp;
-for (int i = 0; i < nums.Length - 1; i++)
-{
-    for (int j = i + 1; j < nums.Length; j++)
-    {
-        if (nums[i] > nums[j])
-        {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
-    }
+    return n * Factorial(n - 1);
 }
 
-// вывод
-Console.WriteLine("Вывод отсортированного массива");
-for (int i = 0; i < nums.Length; i++)
+int factorial4 = Factorial(4);  // 24
+int factorial5 = Factorial(5);  // 120
+int factorial6 = Factorial(6);  // 720
+
+Console.WriteLine($"Факториал числа 4 = {factorial4}");
+Console.WriteLine($"Факториал числа 5 = {factorial5}");
+Console.WriteLine($"Факториал числа 6 = {factorial6}");
+
+/*
+ Сначала идет проверка, равно ли число единице:
+
+1
+if (n == 1) return 1;
+Но вначале n равно 4, поэтому это условие ложно, и соответственно выполняется код
+
+1
+return n * Factorial(n - 1);
+То есть фактически мы имеем:
+
+1
+return 4 * Factorial(3);
+Далее выполняется выражение:
+
+1
+Factorial(3)
+Опять же n не равно 1, поэтому выполняется код
+
+1
+return n * Factorial(n - 1);
+То есть фактически:
+
+1
+return 3 * Factorial(2);
+Далее выполняется выражение:
+
+1
+Factorial(2)
+Опять же n не равно 1, поэтому выполняется код
+
+1
+return n * Factorial(n - 1);
+То есть фактически:
+
+1
+return 2 * Factorial(1);
+Далее выполняется выражение:
+
+1
+Factorial(1)
+Теперь n равно 1, поэтому выполняется код
+
+1
+if (n == 1) return 1;
+И возвращается 1.
+
+В итоге выражение
+
+1
+Factorial(4)
+В реальности выливается в
+
+1
+4 * 3 * 2 * Factorial(1)
+ */
+
+int Fibonachi(int n)
 {
-    Console.WriteLine(nums[i]);
+    if (n == 0 || n == 1) return n;
+
+    return Fibonachi(n - 1) + Fibonachi(n - 2);
 }
-// Тут тоже не понятно, для чего дан пример, мб для понимания работы сортировки, но все же указать, что есть встроенный метод для сортировки не помешало бы Array.Sort
-int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
-Array.Sort(nums);
-foreach (int number in nums)
-    { 
-    Console.WriteLine(number); 
-}
+int fib1 = Fibonachi(1);
+int fib2 = Fibonachi(2);
+int fib3 = Fibonachi(3);
+int fib4 = Fibonachi(4);
+int fib5 = Fibonachi(5);
+int fib6 = Fibonachi(6);
+
+Console.WriteLine($"4 число Фибоначчи = {fib1}");
+Console.WriteLine($"5 число Фибоначчи = {fib2}");
+Console.WriteLine($"6 число Фибоначчи = {fib3}");
+Console.WriteLine($"4 число Фибоначчи = {fib4}");
+Console.WriteLine($"5 число Фибоначчи = {fib5}");
+Console.WriteLine($"6 число Фибоначчи = {fib6}");
+
+
+
+// В статье на метанит не указан, как по мне основной момент, благо есть гпт)) 
+// Ваш метод является рекурсивным, поскольку вызывает сам себя до тех пор, пока не выполнится базовое условие (if (n == 1)).
+// С этой строкой выше в целом пришло понимание что есть рекурсия
